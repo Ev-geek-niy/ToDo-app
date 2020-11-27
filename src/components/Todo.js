efe0d560bc90efe7a9bda5faf5f211d2f3f8a979
Saladerk <44067675+Saladerk@users.noreply.github.com>
2020-11-28 00:27:35 +0500

    Revert "initial commit"
    This reverts commit 469a4e8dd296e12034aa59d7baa975cf983e74ab.

--- src/components/Todo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default props => (
-    <div>
-        <div>{props.todo.text}</div>
-        <button onClick={props.toggleComplete}>✅</button>
-        <button onClick={props.handleDelete}>❌</button>
-    </div>
-);
